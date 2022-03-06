a44be793424669781f5af33251554d41c6ed61e8
Dylan K <drkunkel7@gmail.com>
2022-03-06 14:36:25 -0600

    knock-off wordle is a success! time to make it better and clean it up

--- script.js
+++ script.js
@@ -3,23 +3,54 @@ const randomWords = require(`random-words`);
 const submitBtn = document.querySelector(".submit");
 
 // console.log(randomWords({ exactly: 1, maxLength: 5}))
+function displayResult(guess, word) {
+    // console.log(word + " word")
+    // console.log(guess + " guess")
+    const guessArr = guess.split("");
+    const wordArr = word.toString().split("");
+    console.log(guessArr);
+    const newH1 = document.createElement("h1");
+    document.body.appendChild(newH1);
+    for (i = 0; i < guessArr.length; i++) {
+        const letterSpan = document.createElement("span");
+        letterSpan.classList.add(`letter-${i}-${count}`)
+        const spanContent = document.createTextNode(guessArr[i]);
+        letterSpan.appendChild(spanContent);
+        newH1.appendChild(letterSpan);
+    }
+    for (i = 0; i < guessArr.length; i++) {
+        if (wordArr.includes(guessArr[i])) {
+            console.log("success")
+            console.log(wordArr.indexOf(guessArr[i]) + " " + guessArr.indexOf(guessArr[i]))
+            if (wordArr.indexOf(guessArr[i]) === guessArr.indexOf(guessArr[i])) {
+                const letterEl = document.querySelector(`.letter-${i}-${count}`);
+                letterEl.classList.add("correct-position")
+            } else {
+                const letterEl = document.querySelector(`.letter-${i}-${count}`);
+                letterEl.classList.add("correct-letter")
+            }
+        }
+    }
+}
 
-
-function getWord() {
+// function getWord() {
     let word = randomWords({exactly: 1, maxLength: 5});
-    console.log(word);
-    console.log(word[0].length)
+    // console.log(word);
+    // console.log(word[0].length)
     while (word[0].length < 5) {
         word = randomWords({exactly: 1, maxLength: 5});
         console.log(word + " in loop")
     }
 
     console.log(word + " final")
-}
+// }
 
-getWord();
+let count = 0;
 
 submitBtn.addEventListener("click", function() {
+    count++
     const guess = document.querySelector("#guess").value;
-    console.log(guess);
+    console.log(guess + " | " + word);
+
+    displayResult(guess, word, count)
 })
